acab541be70de4149bdc14bbb42cb4178d19b833
Tim Roberts <tim.roberts20@imperial.ac.uk>
2023-08-22 10:35:30 +0100

    📝 update docs to reflect changes

--- src/utils/getAdjustedLimit.ts
+++ src/utils/getAdjustedLimit.ts
@@ -14,10 +14,19 @@ export const getAdjustedLimit = (
         };
     }
 
+    // guard against limits that are out of bounds
     const adjustedMaxLimit = limit.max
         ? Math.min(limit.max, numberOfItems)
         : numberOfItems;
     const adjustedMinLimit = limit.min ? Math.max(limit.min, 0) : 0;
+
+    // guard against invalid limits
+    if (adjustedMaxLimit < adjustedMinLimit) {
+        return {
+            max: numberOfItems,
+            min: 0,
+        };
+    }
 
     return {
         max: adjustedMaxLimit,
