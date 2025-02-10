7e1c1a5c9362c2ea49c063d98176298c16c9f004
Tim Roberts <timtroberts28@gmail.com>
2025-02-10 12:24:45 +0000

    📝 ➕ add `MaskedView` dependency to example and update example app to show usage

--- examples/example-expo/App.tsx
+++ examples/example-expo/App.tsx
@@ -11,6 +11,7 @@ import { Audio } from "expo-av";
 import * as Haptics from "expo-haptics";
 import { LinearGradient } from "expo-linear-gradient";
 import {
+    Image,
     LayoutAnimation,
     Platform,
     Pressable,
@@ -27,6 +28,7 @@ import type { NativeScrollEvent, NativeSyntheticEvent } from "react-native";
 import { TimerPicker, TimerPickerModal } from "../../src";
 
 import { formatTime } from "./utils/formatTime";
+import MaskedView from "@react-native-masked-view/masked-view";
 
 if (Platform.OS === "android") {
     UIManager.setLayoutAnimationEnabledExperimental?.(true);
@@ -118,6 +120,7 @@ export default function App() {
                         setAlarmStringExample1(formatTime(pickedDuration));
                         setShowPickerExample1(false);
                     }}
+                    hideHours
                     setIsVisible={setShowPickerExample1}
                     styles={{
                         theme: "dark",
@@ -187,7 +190,10 @@ export default function App() {
 
     const renderExample3 = useMemo(() => {
         return (
-            <View
+            <LinearGradient
+                colors={["#202020", "#220578"]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
                 style={[
                     styles.container,
                     styles.page3Container,
@@ -198,12 +204,13 @@ export default function App() {
                     Haptics={Haptics}
                     hourLabel=":"
                     LinearGradient={LinearGradient}
+                    MaskedView={MaskedView}
                     minuteLabel=":"
                     padWithNItems={2}
                     secondLabel=""
                     styles={{
                         theme: "dark",
-                        backgroundColor: "#202020",
+                        backgroundColor: "transparent",
                         pickerItem: {
                             fontSize: 34,
                         },
@@ -226,7 +233,7 @@ export default function App() {
                         },
                     }}
                 />
-            </View>
+            </LinearGradient>
         );
     }, [screenWidth]);
 
@@ -363,7 +370,7 @@ const styles = StyleSheet.create({
         backgroundColor: "#F1F1F1",
     },
     page3Container: {
-        backgroundColor: "#202020",
+        flex: 1,
     },
     page4Container: {
         backgroundColor: "#F1F1F1",
